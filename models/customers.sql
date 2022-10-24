cf5557b7811bf1a52898de6131e024d2737d75fa
Rafaela Lorenzini <rafaela.lorenzini@gmail.com>
2022-10-24 00:22:06 +0000

    update customers

--- models/customers.sql
+++ models/customers.sql
@@ -1,3 +1,4 @@
+--model customers
 with markup as (select *, 
 first_value(customer_id) over(partition by company_name,contact_name order by company_name
 rows between unbounded preceding and unbounded following) as result 
